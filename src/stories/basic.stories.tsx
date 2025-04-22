c994d796f9a7bf8c7ef74a2f14e944f473a6b60e
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2025-04-22 11:42:26 -0400

    feat: add `NumberInput` component with basic styling and story
    - Create `NumberInput` component with input type number
    - Apply basic styles to `NumberInput` component
    - Export `NumberInput` from its index file
    - Add `NumberInput` to storybook for documentation
    - Ensure consistent styling with CSS module usage

--- src/stories/basic.stories.tsx
+++ src/stories/basic.stories.tsx
@@ -1,5 +1,7 @@
 import Checkbox from "../components/Checkbox";
 import Button from "../components/Button";
+import NumberInput from "../components/NumberInput";
 
 export const CheckboxStory = () => <Checkbox />;
 export const ButtonStory = () => <Button>Button</Button>;
+export const NumberInputStory = () => <NumberInput />;
