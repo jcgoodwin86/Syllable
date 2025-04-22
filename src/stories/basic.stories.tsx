a1a1ba8ef2cce24bfd6a236128f6fbcfea7aa896
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2025-04-22 11:04:22 -0400

    feat: add reusable `Button` component with basic styles
    - Implement Button component with customizable className
    - Create CSS module for Button styles
    - Define Button hover and active states
    - Export Button component from index file
    - Add Button component to stories

--- src/stories/basic.stories.tsx
+++ src/stories/basic.stories.tsx
@@ -1,3 +1,5 @@
 import Checkbox from "../components/Checkbox";
+import Button from "../components/Button";
 
 export const CheckboxStory = () => <Checkbox />;
+export const ButtonStory = () => <Button>Button</Button>;
